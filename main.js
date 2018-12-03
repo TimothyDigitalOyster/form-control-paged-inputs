fe6608d1bb221b230fff44b20d0c7cb767303062
Timothy Digital Oyster <timothy@digitaloyster.uk>
2018-12-03 11:43:38 +0000

    Initial Commit - console messages on control clicks

---
+++ main.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+    var plusButton = $('button['data-id="+"']');
+    var minusButton = $('button['data-id="-"']');
+
+    plusButton.on("click", function() {
+      console.log("+");
+    });
+
+    minusButton.on("click", function() {
+      console.log("-");
+    });
+});
