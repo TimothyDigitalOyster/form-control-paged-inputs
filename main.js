e3f6ccbf306ca39837ef3c2f346ed246751b8d5c
Timothy Digital Oyster <timothy@digitaloyster.uk>
2018-12-03 12:10:23 +0000

    Added some logic to the controls, added element.html for reference

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 $( document ).ready(function() {
     var plusButton = $('button[data-id="+"]');
     var minusButton = $('button[data-id="-"]');
+    var numbers = $('.form-element-numbers')
+    var pages = $('form-element-pages');
+    var getPages = $('.form-element-input').size();
 
     plusButton.on("click", function() {
-      console.log("+");
+      var newPage = getPages + 1;
+      $('.active').removeClass('active');
+      numbers.append("<a href='#' class='active' data-id="+newPage+">"+newPage+"</a>");
+      pages.append("<div class='form-element-input active" data-id="+newPage+"><input type='text' class='input' id='former-name-"+newPage+"' /></div>");
     });
 
     minusButton.on("click", function() {
-      console.log("-");
+      $('.form-element-container .active').remove();
+      numbers[0].addClass('active');
+      pages[0].addClass('active');
     });
 });
